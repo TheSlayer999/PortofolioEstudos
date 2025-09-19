8b73e6c9b49a12b05d85c0a08463bee0f39219f6
TheSlayer999 <davidjesus200813@gmail.com>
2025-09-19 21:53:55 +0100

    Create Soma.py
    Aula introdutória de API, algo básico envolvendo soma em py

---
+++ Aulas API B/Aula n1/Soma.py
@@ -0,0 +1,20 @@
+
+#print serve para escrever
+
+#input cria um campo
+
+print("Introduza um número:")
+n1=int(input())
+
+print("Introduza outro número:")
+n2=int(input())
+
+#int numero normal
+
+#float paradecimal
+
+soma=n1+n2
+
+
+
+print(n1," + ",n2," = ",soma)
