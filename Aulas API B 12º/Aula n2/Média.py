2046c9f4ff2a03c800b689540209ab5fcb16fb3e
TheSlayer999 <davidjesus200813@gmail.com>
2025-09-26 17:00:52 +0100

    media e boas vindas
    introduçao do if e do else e um detalhe do f antes da escrita

--- Aulas API B 12º/Aula n2/Média.py
+++ Aulas API B 12º/Aula n2/Média.py
@@ -1,4 +1,31 @@
 n1=float(input("Escreve a nota do teste nº1: "))
 n2=float(input("Escreve a nota do teste nº2: "))
+
 media=(n1+n2)/2
-print("A tua média é: ",media)
+
+""" print("A tua média é: ",media) ou
+print(f"A tua média é: {media}") """
+
+if media>=0 and media<9.5: 
+    print(f"A tua média é: {media}")
+    print("Ups! Tiveste negativa!")
+
+elif media>=9.5 and media<14.5:
+    print(f"A tua média é: {media}")
+    print("Passaste! Tiveste suficiente.")
+
+elif media>=14.5 and media<17.5:
+    print(f"A tua média é: {media}")
+    print("Boa! Tiveste bom!")
+
+elif media>=17.5 and media<=20:
+    print(f"A tua média é: {media}")
+    print("Parabéns! Tiveste excelente!")
+
+else:
+    print("ERRO")
+    
+
+#if é igual a se
+#elif se quisermos adicionar mais ses
+#else é igual a senão
---
+++ Aulas API B 12º/Aula n3/boasvindas.py
@@ -0,0 +1,8 @@
+nome=input("Introduza o seu nome: ")
+idade=int(input("Introduza a sua idade: "))
+
+print("********************************************")
+#print("Olá! Eu sou o",nome,"e tenho",idade,"anos!!!") ou
+print(f"Olá! Eu sou o {nome} e tenho {idade} anos!!!")
+
+#o f permite misturar tudo, variáveis e texto
