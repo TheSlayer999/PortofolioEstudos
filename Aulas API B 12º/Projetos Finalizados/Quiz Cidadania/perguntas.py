260f21841bd15c7669ec8486ab187a33af28d24a
TheSlayer999 <davidjesus200813@gmail.com>
2026-02-01 21:29:37 +0000

    Quiz de Cidadania

---
+++ Aulas API B 12º/Projetos Finalizados/Quiz Cidadania/perguntas.py
@@ -0,0 +1,33 @@
+pr = [{"pergunta": "O que são os Direitos Humanos?", 
+       "respostas": ["a - Direitos concedidos apenas a cidadãos de um país", "b - Direitos exclusivos de grupos específicos","c - Direitos básicos garantidos a todas as pessoas","d - Direitos ligados apenas ao trabalho"], "certa": "c"},
+
+      {"pergunta": "Qual documento proclamou oficialmente os Direitos Humanos em 1948?", 
+       "respostas": ["a - Constituição dos EUA", "b - Tratado de Versalhes","c - Declaração Universal dos Direitos Humanos","d - Carta das Nações Unidas"], "certa": "c"},
+
+      {"pergunta": "Quem é responsável por garantir os Direitos Humanos?", 
+       "respostas": ["a - Apenas a ONU", "b - Apenas os governos","c - Apenas os cidadãos","d - Governos, instituições e a sociedade"], "certa": "d"},
+
+      {"pergunta": "Qual destes é um direito humano fundamental?", 
+       "respostas": ["a - Direito a férias pagas", "b - Direito à vida","c - Direito a lucros empresariais","d - Direito a cargos políticos"], "certa": "b"},
+
+      {"pergunta": "Os Direitos Humanos aplicam-se a:", 
+       "respostas": ["a - Apenas adultos", "b - Apenas cidadãos nacionais","c - Todas as pessoas, sem exceção","d - Apenas pessoas em situação de pobreza"], "certa": "c"},
+
+      {"pergunta": "Qual destes princípios está associado aos Direitos Humanos?", 
+       "respostas": ["a - Desigualdade", "b - Discriminação","c - Universalidade","d - Exclusividade"], "certa": "c"},
+
+      {"pergunta": "Qual organização internacional tem como missão promover os Direitos Humanos?", 
+       "respostas": ["a - FIFA", "b - União Europeia","c - Organização das Nações Unidas (ONU)","d - Organização Mundial do Comércio"], "certa": "c"},
+
+      {"pergunta": "A discriminação é:", 
+       "respostas": ["a - Permitida em certas situações", "b - Um direito individual","c - Contrária aos Direitos Humanos","d - Incentivada pela lei"], "certa": "c"},
+
+      {"pergunta": "Quais são as principais características dos Direitos Humanos?", 
+       "respostas": ["a - Concedidos pelos Estados conforme a sua cultura", "b - Universais, inalienáveis e indivisíveis","c - Dependentes do desenvolvimento económico dos países","d - Aplicáveis apenas em regimes democráticos"], "certa": "b"},
+
+      {"pergunta": "Qual entidade internacional tem competência para julgar crimes contra a humanidade?", 
+       "respostas": ["a - Conselho da Europa", "b - Tribunal Europeu dos Direitos Humanos","c - Tribunal Penal Internacional","d - Assembleia Geral da ONU"], "certa": "c"}
+      ]
+
+def creditos():
+    print("Programa criado em Python por David Jesus 12ºCTC\nObrigado por usar o programa!")
---
+++ Aulas API B 12º/Projetos Finalizados/Quiz Cidadania/quiz.py
@@ -0,0 +1,46 @@
+import os
+from perguntas import *
+import random
+from time import sleep
+
+def embaralhar_quiz(perguntas):
+    random.shuffle(perguntas)
+
+    """ for pergunta in perguntas:
+        resposta_certa = pergunta["respostas"][pergunta["certa"]]
+        random.shuffe(pergunta["respostas"])
+        pergunta["certa"] = pergunta["respostas"].index(resposta_certa) """
+
+embaralhar_quiz(pr)
+n_perguntas=10
+certas=0
+erradas=0
+
+for i in range (len(pr)):
+    os.system("cls" if os.name=="nt" else"clear")
+    print("--- Quiz de Cidadania ---\n")
+    print(f"{i+1} - {pr[i]['pergunta']}\n")
+    for o in range (len(pr[i]["respostas"])):
+        print(f"{pr[i]['respostas'][o]}")
+    resposta=input("\nQual a resposta certa?: ")
+    while resposta.lower()!="a" and resposta.lower()!="b" and resposta.lower()!="c" and resposta.lower()!="d":
+        print("\nResposta inválida!")
+        resposta=input("Qual a resposta certa?: ")
+
+    if resposta.lower() == pr[i]["certa"][0]:
+        certas+=1
+    else:
+        erradas+=1
+        
+
+score=n_perguntas-erradas
+os.system("cls" if os.name=="nt" else"clear")
+print("--- Quiz de Cidadania ---")
+print(f"\nParabéns por concluires o quiz!\nA tua pontuação foi: {score}/{n_perguntas}")
+sair=input("\nPressione para qualquer tecla para encerrar...")
+
+os.system("cls" if os.name=="nt" else"clear")
+print("--- Quiz de Cidadania ---\n")
+creditos()
+sleep(3)
+os.system("cls" if os.name=="nt" else"clear")
